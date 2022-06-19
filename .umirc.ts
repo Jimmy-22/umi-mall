8e7dadbec176f80ed2f4fbc75b7209ca411304ae
ferrytsui <ferrytsui@163.com>
2022-06-19 23:49:07 +0800

    first commit

---
+++ .umirc.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', component: '@/pages/index' },
+    { path: '/cart', component: '@/pages/cart/index' },
+    { path: '/login', component: '@/pages/login/index' },
+    { path: '/order-list', component: '@/pages/order-list/index' },
+    { path: '/user', component: '@/pages/user/index' },
+  ],
+  fastRefresh: {},
+});
